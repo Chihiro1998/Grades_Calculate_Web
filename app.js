580e0b2c9b9ed5311318c9ede86dd64fd9c57ab5
Chihiro1998 <935608397@qq.com>
2024-09-30 01:06:18 -0400

    Change the select color for ABCDF level

--- app.js
+++ app.js
@@ -31,3 +31,61 @@ time_line
 setTimeout(() => {
   animation.style.pointerEvents = "none";
 }, 2500);
+
+// Disable EnterKey in our web
+window.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+  }
+});
+
+// Prevent the Button inside the Form from handing over the form.
+let allButtons = document.querySelectorAll("button");
+allButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+// Change the select Color for ABCDEF Level
+let allSelects = document.querySelectorAll("select");
+allSelects.forEach((select) => {
+  select.addEventListener("change", (e) => {
+    setGPA();
+    // console.log(e.target.value); //Console can see the value
+    changeColor(e.target);
+  });
+});
+function changeColor(target) {
+  if (target.value == "A" || target.value == "A-") {
+    target.style.backgroundColor = "lightgreen";
+    target.style.color = "black";
+  } else if (
+    target.value == "B+" ||
+    target.value == "B" ||
+    target.value == "B-"
+  ) {
+    target.style.backgroundColor = "yellow";
+    target.style.color = "black";
+  } else if (
+    target.value == "C+" ||
+    target.value == "C" ||
+    target.value == "C-"
+  ) {
+    target.style.backgroundColor = "orange";
+    target.style.color = "black";
+  } else if (
+    target.value == "D+" ||
+    target.value == "D" ||
+    target.value == "D-"
+  ) {
+    target.style.backgroundColor = "red";
+    target.style.color = "black";
+  } else if (target.value == "F") {
+    target.style.backgroundColor = "grey";
+    target.style.color = "white";
+  } else {
+    target.style.backgroundColor = "White";
+  }
+}
+function setGPA() {}
